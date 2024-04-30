ca8770e00d0c6cf069d104052fc8f979df12765c
Leziga Bakor <leziga.bakor@gmail.com>
2024-04-30 18:32:06 +0100

    Added database setup

--- companyBlog/__init__.py
+++ companyBlog/__init__.py
@@ -1,6 +1,19 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+####################################
+### DATABSASE SETUP ################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABSASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
 
 from companyBlog.core.views import core
 from companyBlog.error_pages.handlers import error_pages

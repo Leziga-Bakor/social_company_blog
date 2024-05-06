6a681eb3097aeb1d37952d013006f64a650805e3
Leziga Bakor <leziga.bakor@gmail.com>
2024-05-06 23:38:02 +0100

    Added route for logout and register

--- companyBlog/Users/views.py
+++ companyBlog/Users/views.py
@@ -6,3 +6,28 @@ from companyBlog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from companyBlog.users.picture_handler import add_profile_pic
 
 users = Blueprint('users',__name__)
+
+# register
+@users.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash('Thank you for registering')
+        return redirect(url_for('user.login'))
+    
+    return render_template('register.html',form=form)
+
+# login
+
+# logout
+@users.route("/logout")
+def logout():
+    login_user()
+    return redirect(url_for("core.index"))

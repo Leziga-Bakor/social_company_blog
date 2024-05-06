3c903b84a5dd6298b69dbbbb572b696506c6dcde
Leziga Bakor <leziga.bakor@gmail.com>
2024-05-06 23:27:03 +0100

    Updated folder name and Added imports for user views

---
+++ companyBlog/Users/views.py
@@ -0,0 +1,6 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from companyBlog import db
+from companyBlog.models import User, BlogPost
+from companyBlog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
+from companyBlog.users.picture_handler import add_profile_pic

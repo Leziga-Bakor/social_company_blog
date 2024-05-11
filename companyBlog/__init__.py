f67a95f5beed8b7d78e46e365ae6df55b3f8319f
Leziga Bakor <leziga.bakor@gmail.com>
2024-05-11 11:32:34 +0100

    Corrected database spelling

--- companyBlog/__init__.py
+++ companyBlog/__init__.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY']='mysecret'
 ####################################
 ######### DATABSASE SETUP ##########
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABSASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)

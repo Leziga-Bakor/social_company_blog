f951121b608a2240b5e860200493f3b79c4835c2
Leziga Bakor <leziga.bakor@gmail.com>
2024-05-14 11:04:24 +0100

    Added and registered blueprint from blogpost

--- companyBlog/__init__.py
+++ companyBlog/__init__.py
@@ -40,8 +40,10 @@ login_manager.login_view = "users.login"
 from companyBlog.core.views import core
 from companyBlog.error_pages.handlers import error_pages
 from companyBlog.users.views import users
+from companyBlog.blog_post.views import blog_posts
 
 # Registration of blueprints
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)

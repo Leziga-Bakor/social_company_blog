876de1983d2b567229aabc54f2497a73f9c12bb1
Leziga Bakor <leziga.bakor@gmail.com>
2024-05-21 12:33:50 +0100

    get blog posts and pass it to index page

--- companyBlog/core/views.py
+++ companyBlog/core/views.py
@@ -9,7 +9,7 @@ core = Blueprint('core',__name__)
 def index():
     page = request.args.get('page', 1, type=int)
     blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
-    return render_template('index.html', blog_posts)
+    return render_template('index.html', blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
